b7b017131b2358b6d4c2bfd99f1473afd276879c
Prince Raj <princeraj427@gmai.com>
2019-08-17 20:28:25 +0530

    Final Project

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,14 @@ class Blog(models.Model):
     # image
     image = models.ImageField(upload_to='images/')
 
+    def __str__(self):
+        return self.title
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%d-%b-%Y')
+
 
 # Add the blog app to the settings, then 
 
